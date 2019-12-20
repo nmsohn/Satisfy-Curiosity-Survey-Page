1775c26eb4e6157011271aeebaf03c6339698637
Hahn <hahn.son@optimationgroup.com>
2019-12-20 14:19:54 +1300

    less file loading fails

--- next.config.js
+++ next.config.js
@@ -1,58 +1,80 @@
-const withLess = require('@zeit/next-less');
-const lessToJS = require('less-vars-to-js');
-const withTypescript = require('@zeit/next-typescript');
-const fs = require('fs');
-const path = require('path');
+const withCss = require("@zeit/next-css");
+const withLess = require("@zeit/next-less");
+const lessToJS = require("less-vars-to-js");
+const withTypescript = require("@zeit/next-typescript");
+const fs = require("fs");
+const path = require("path");
 const withPlugins = require("next-compose-plugins");
-const recursiveCopy = require('recursive-copy');
+const recursiveCopy = require("recursive-copy");
+const withImages = require("next-images");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const antdLessLoader = require("next-antd-aza-less");
+const withLessExcludeAntd = require("./next-less.config.js");
 
-const themeVariables = lessToJS(
-  fs.readFileSync(path.resolve(__dirname, './assets/antd-custom.less'), 'utf8')
-)
+const themeVariables = lessToJS(fs.readFileSync(path.resolve(__dirname, "./assets/antd-custom.less"), "utf8"));
+const cssConfig = {
+	cssLoaderOptions: {
+		importLoaders: 1,
+		localIdentName: "[local]___[hash:base64:5]"
+	}
+};
+const lessConfig = {
+	cssModules: true,
+	import: true,
+	cssLoaderOptions: {
+		importLoaders: 1,
+		localIdentName: "[local]___[hash:base64:5]"
+	},
+	LessLoaderOptions: {
+		javascriptEnabled: true,
+		modifyVars: themeVariables // make your antd custom effective
+	}
+};
+const nextConfig = {
+	// exportPathMap: async function (defaultPathMap, {dev, dir, outDir, distDir, buildId}) {
+	//   await recursiveCopy(join(dir, 'src/'), outDir, {dot:true});
+	//   return defaultPathMap
+	// },
+	webpack: (config, { isServer }) => {
+		if (isServer) {
+			const antStyles = /antd\/.*?\/style.*?/;
+			const origExternals = [...config.externals];
+			config.externals = [
+				(context, request, callback) => {
+					if (request.match(antStyles)) return callback();
+					if (typeof origExternals[0] === "function") {
+						origExternals[0](context, request, callback);
+					} else {
+						callback();
+					}
+				},
+				...(typeof origExternals[0] === "function" ? [] : origExternals)
+			];
 
-if (typeof require !== 'undefined') {
-  require.extensions['.less'] = file => {}
+			config.module.rules.unshift({
+				test: antStyles,
+				use: "null-loader"
+			});
+
+			// config.plugins.push(
+			// 	new MiniCssExtractPlugin({
+			// 		// Options similar to the same options in webpackOptions.output
+			// 		// both options are optional
+			// 		filename: "[name].css",
+			// 		chunkFilename: "[id].css"
+			// 	})
+			// );
+		}
+
+		return config;
+	}
+};
+
+if (typeof require !== "undefined") {
+	require.extensions[".less"] = file => {};
 }
 
-module.exports = withPlugins(
-  [
-    withTypescript,
-    withLess
-  ],
-  {
-    LessLoaderOptions: {
-      javascriptEnabled: true,
-      modifyVars: themeVariables // make your antd custom effective
-    },
-    // exportPathMap: async function (defaultPathMap, {dev, dir, outDir, distDir, buildId}) {
-    //   await recursiveCopy(join(dir, 'src/'), outDir, {dot:true});
-    //   return defaultPathMap
-    // },
-    webpack: (config, { isServer }) => {
-      if (isServer) {
-        const antStyles = /antd\/.*?\/style.*?/
-        const origExternals = [...config.externals]
-        config.externals = [
-          (context, request, callback) => {
-            if (request.match(antStyles)) return callback()
-            if (typeof origExternals[0] === 'function') {
-              origExternals[0](context, request, callback)
-            } else {
-              callback()
-            }
-          },
-          ...(typeof origExternals[0] === 'function' ? [] : origExternals),
-        ]
-  
-        config.module.rules.unshift({
-          test: antStyles,
-          use: 'null-loader',
-        })
-      }
-      return config
-    },
-  }
-)
+module.exports = withPlugins([[withImages], [withLess, lessConfig], nextConfig]);
 
 // module.exports = withTypescript(withLess({
 //   lessLoaderOptions: {
--- pages/Index/index.js
+++ pages/Index/index.js
@@ -1,12 +1,12 @@
-import Head from "next/head"
-import { Layout, Row } from "antd"
-import { Header } from "../../components/Header"
+import Head from "next/head";
+import { Layout, Row } from "antd";
+import { Header } from "../../components/Header";
 
-const { Content } = Layout
+const { Content } = Layout;
 
-export default ({data}) => (
-    <>
-        <Helmet title="Home | LAH" />
-
-    </>
-)
+export default ({ data }) => (
+	<div>
+		<Helmet title="Home | LAH" />
+		<Header centerColumn={<h4>Page Name</h4>} rightColumn={<h4>Menu</h4>} leftColumn={<h4>Logo</h4>} />
+	</div>
+);
