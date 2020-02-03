fd0978246bf75c94e51c71a2699704be76eea6c6
Hahn <hahn.son@optimationgroup.com>
2020-02-03 16:30:59 +1300

    clean config files

--- next.config.js
+++ next.config.js
@@ -13,12 +13,6 @@ const withAntd = require("./next-antd.config");
 
 const themeVariables = lessToJS(fs.readFileSync(path.resolve(__dirname, "./assets/less/antd-custom.less"), "utf8"));
 
-// const cssConfig = {
-// 	cssLoaderOptions: {
-// 		importLoaders: 1,
-// 		localIdentName: "[local]___[hash:base64:5]"
-// 	}
-// };
 const lessConfig = {
 	cssModules: true,
 	cssLoaderOptions: {
@@ -31,40 +25,7 @@ const lessConfig = {
 	}
 };
 const nextConfig = {
-	// exportPathMap: async function (defaultPathMap, {dev, dir, outDir, distDir, buildId}) {
-	//   await recursiveCopy(join(dir, 'src/'), outDir, {dot:true});
-	//   return defaultPathMap
-	// },
 	webpack: config => {
-		// if (isServer) {
-		// 	const antStyles = /antd\/.*?\/style.*?/;
-		// 	const origExternals = [...config.externals];
-		// 	config.externals = [
-		// 		(context, request, callback) => {
-		// 			if (request.match(antStyles)) return callback();
-		// 			if (typeof origExternals[0] === "function") {
-		// 				origExternals[0](context, request, callback);
-		// 			} else {
-		// 				callback();
-		// 			}
-		// 		},
-		// 		...(typeof origExternals[0] === "function" ? [] : origExternals)
-		// 	];
-		// 	config.module.rules.unshift({
-		// 		test: antStyles,
-		// 		use: "null-loader"
-		// 	});
-
-		// 	// config.plugins.push(
-		// 	// 	new MiniCssExtractPlugin({
-		// 	// 		// Options similar to the same options in webpackOptions.output
-		// 	// 		// both options are optional
-		// 	// 		filename: "[name].css",
-		// 	// 		chunkFilename: "[id].css"
-		// 	// 	})
-		// 	// );
-		// }
-
 		config.plugins.push(
 			new FilterWarningsPlugin({
 				// ignore ANTD chunk styles [mini-css-extract-plugin] warning
@@ -80,17 +41,3 @@ if (typeof require !== "undefined") {
 }
 
 module.exports = withPlugins([[withImages], [withAntd, lessConfig], nextConfig]);
-
-// module.exports = withTypescript(withLess({
-//   lessLoaderOptions: {
-//       javascriptEnabled: true,
-//   },
-//   exportPathMap: () => return {
-//     '/': { page: '/' },
-//     '/about': { page: '/about' },
-//     '/readme.md': { page: '/readme' },
-//     '/p/hello-nextjs': { page: '/post', query: { title: 'hello-nextjs' } },
-//     '/p/learn-nextjs': { page: '/post', query: { title: 'learn-nextjs' } },
-//     '/p/deploy-nextjs': { page: '/post', query: { title: 'deploy-nextjs' } }
-// },
-// }));
