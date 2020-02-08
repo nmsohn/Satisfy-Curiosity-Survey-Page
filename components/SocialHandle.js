4ca788eb607d52d98ed0987718c9f53b04e91c15
Hahn <hahn.son@optimationgroup.com>
2020-02-08 13:44:00 +1300

    Removed some icon libraries

--- components/SocialHandle.js
+++ components/SocialHandle.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
-import { FacebookSquare } from 'styled-icons/boxicons-logos/FacebookSquare';
+import React, { Component } from "react";
+import { FacebookSquare } from "styled-icons/boxicons-logos/FacebookSquare";
+import { Mail } from "styled-icons/material/Mail";
 import styled from "styled-components";
 import { Col } from "antd";
 
 const SocialCol = styled(Col)`
-{
-    width: 100%;
-}
+	 {
+		width: 100%;
+	}
 `;
 
 const FacebookIcon = styled(FacebookSquare)`
-    margin: 0 auto;
-    align-items: center;
-    display: flex;
-    color: "#fff";
-    font-size: "30px";
+	margin: 20px auto;
+	align-items: center;
+	display: flex;
+	color: "#fff";
 `;
 
 export default class SocialHandle extends Component {
-    render() {
-        return (
-            <SocialCol span={24}>
-                <FacebookIcon />
-            </SocialCol>
-        )
-    }
+	render() {
+		return (
+			<SocialCol span={24}>
+				<a href="#" style={{ color: "#fff" }}>
+					<FacebookIcon size={38} />
+				</a>
+			</SocialCol>
+		);
+	}
 }
