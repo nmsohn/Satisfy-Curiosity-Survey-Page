977c879ee0d10de2268af7c5db690e556b5f718b
Hahn <hahn.son@optimationgroup.com>
2020-02-11 09:59:56 +1300

    Removed unused vars

--- components/ErrorPage.js
+++ components/ErrorPage.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 
-import ErrorPage from '../components/ErrorPage';
-
 export default class RootError extends Component {
 	render() {
 		const { statusCode } = this.props;
-		return <ErrorPage statuscode={statusCode} />;
+		return <div statuscode={statusCode} />;
 	}
 }
